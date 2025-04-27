895ddf6ee53d64dfbad93471e30c5382ffce97c4
ug <ugurgunes@windowslive.com>
2025-04-27 18:44:40 +0300

    Update api.ts

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,4 +1,4 @@
-const API_URL = process.env.REACT_APP_API_URL || "http://127.0.0.1:8000/api";
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000/api";
 
 export async function addTeam(name: string, power: number) {
   const res = await fetch(`${API_URL}/teams`, {
